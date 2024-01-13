442f17267fc0369417fface57a9c6daa2bcc2b7c
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-01-13 17:29:47 +0500

    refactored

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -8,9 +8,9 @@ const GameTrailer = ({ gameId }: Props) => {
   if (isLoading) return null;
   if (error) throw error;
 
-  const first = data?.results[1];
+  const first = data?.results[0];
   return first ? (
-    <video src={first.data[480]} poster={first.preview} controls />
+    <video src={first.data.max} poster={first.preview} controls />
   ) : null;
 };
 

0b5718b5bb0bd3c6ab8169f67da6bb02c9d700f6
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-01-13 22:38:02 +0500

    fix the issue with navbar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,16 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
       <HStack justifyContent="space-between" marginTop={1}>
-        <Image src={logo} boxSize="60px " cursor="pointer" marginX={2} />
+        <Link to="/">
+          <Image src={logo} boxSize="60px " cursor="pointer" marginX={2} />
+        </Link>
+
         <SearchInput />
         <ColorModeSwitch />
       </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,15 +2,19 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../hooks/store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
-  const setSearchText = useGameQueryStore(s => s.setSeacrchText)
+  const navigate = useNavigate();
+  const setSearchText = useGameQueryStore((s) => s.setSeacrchText);
   const ref = useRef<HTMLInputElement>(null);
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
+        if (ref.current) {
+          setSearchText(ref.current.value), navigate("/");
+        }
       }}
     >
       <InputGroup>
